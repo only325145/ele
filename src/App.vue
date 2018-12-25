284c80a5c660c626b14bee263742a4428f9b3fba
only325145 <1251585756@qq.com>
2018-12-25 23:08:56 +0800

    start ele

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header></Header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评价</div>
+      <div class="tab-item">详情</div>
     </div>
-    <router-view/>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Header from "./pages/header/Header.vue";
+export default {
+  name: "app",
+  components: {
+    Header
   }
-}
+};
+</script>
+<style lang="stylus" scoped>
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: .8rem
+      line-height: .8rem
+      .tab-item
+        flex: 1
+        text-align: center
 </style>
---
+++ src/pages/header/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>this is header</div>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus" scoped></style>

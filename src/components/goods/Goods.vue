a1bc350a279c5f4b9ce0e1a3b07c36567b2f330f
only325145 <1251585756@qq.com>
2019-01-09 22:38:35 +0800

    modified scroll function

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -10,7 +10,7 @@
       </ul>
     </div>
     <div class="foods">
-      <ul ref="foods">
+      <ul ref="foods" >
         <li v-for="(value,key) in goods" :key="key" class="hook">
           <h1 class="title" :ref="value.name">{{value.name}}</h1>
           <ul class="allFood">
@@ -41,88 +41,102 @@
 <script>
 import Axios from "axios";
 import BScroll from "better-scroll";
-  export default {
-    name: "goods",
-    data() {
-      return {
-        goods: [],
-        touchStatus: false,
-        startY: 0,
-        listHeight: [],
-        scrollPosition: 0,
-        index1: 0
-      }
-    },
-    computed: {
+export default {
+  name: "goods",
+  data() {
+    return {
+      goods: [],
+      touchStatus: false,
+      startY: 0,
+      listHeight: [],
+      scrollPosition: 0,
+      index1: 0
+    };
+  },
+  /* computed: {
       currentIndex: {
         get: function() {
-          for(let i = 0; i<this.listHeight.length; i++){
-            let top = this.listHeight[i];
-            let bottom = this.listHeight[i+1];
-            if(!bottom || (this.scrollPosition >= top && this.scrollPosition < bottom))
-            {
-              return this.index = i;
+          if(this.scrollPosition){
+            for(let i = 0; i<this.listHeight.length; i++){
+              let top = this.listHeight[i];
+              let bottom = this.listHeight[i+1];
+              if(!bottom || (this.scrollPosition >= top && this.scrollPosition < bottom))
+              {
+                return i;
+              }
             }
+            return 0;
           }
-          return 0;
+          else{
+            return this.index1;
+          }
         },
         set: function(value) {
           this.index1 = value;
         }
       }
+    }, */
+  methods: {
+    getGoodsInfo() {
+      Axios.get("/data.json").then(this.getGoodsInfoSucc);
     },
-    methods: {
-      getGoodsInfo() {
-        Axios.get("/data.json").then(this.getGoodsInfoSucc);
-      },
-      getGoodsInfoSucc(res) {
-        res = res.data;
-        if (res.ret && res.data) {
-          this.goods = res.data.goods;
-          this.$nextTick(() => {
-            this.initScroll();
-            this.calculateHeight();   //要对原生DOM进行操作的时候应该将函数写在$nextTick回调函数中，直接写在mounted中也会可能获取不到
-          });
-        }
-      },
-      initScroll() {
-        this.menuScroll = new BScroll(".menu", { click: true});
-        this.foodScroll = new BScroll(".foods", {probeType: 3 ,click: true});
-        this.foodScroll.on("scroll", (pos)=> {    //better-scroll区域滚动时获取滚动的高度
-          this.scrollPosition = Math.abs(Math.round(pos.y));
-        })
-      },
-      rightMach(index,e) {    //函数调用时e的实参写成$event
-        const itemText= e.target.innerText;
-        const element = this.$refs[itemText][0];  //this.$refs是一个对象，因为ref绑定的是循环的数据，所以this.$refs对象里面有全部的数据
-                                            //热销榜:"[xxx]",单人精彩套餐:"[xxx]",冰爽饮品限时特惠:"[xxx]"...    this.$refs[itemText][0]获取的是数组里面的DOM元素
-                                            //因为itemText是变量，所以用[]
-        this.foodScroll.scrollToElement(element);
-        
-        this.currentIndex = index;
-        
-      },
-      calculateHeight() {
-        var foodList = this.$refs.foods.getElementsByClassName("hook");
-        let height = 0; 
-        this.listHeight.push(height);
-        for(let i = 0; i<foodList.length; i++){
-          let item = foodList[i];
-          height += item.clientHeight;
-          this.listHeight.push(height);
-        }
+    getGoodsInfoSucc(res) {
+      res = res.data;
+      if (res.ret && res.data) {
+        this.goods = res.data.goods;
+        this.$nextTick(() => {
+          this.calculateHeight(); //要对原生DOM进行操作的时候应该将函数写在$nextTick回调函数中，直接写在mounted中也会可能获取不到
+          this.initScroll();
+        });
       }
     },
-    created() {
-      this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
-      // class名称和每个特定图标编号相对应
+    initScroll() {
+      this.menuScroll = new BScroll(".menu", { click: true });
+      this.foodScroll = new BScroll(".foods", { probeType: 3, click: true });
+      this.foodScroll.on("scroll", pos => {
+        //better-scroll区域滚动时获取滚动的高度
+        this.scrollPosition = Math.abs(Math.round(pos.y));
+      });
     },
-    mounted() {
-      this.getGoodsInfo();
-      
-         //使用ref的元素要在mounted中，所以calculateHeight函数写在此处
+    rightMach(index, e) {
+      //函数调用时e的实参写成$event
+      const itemText = e.target.innerText;
+      const element = this.$refs[itemText][0]; //this.$refs是一个对象，因为ref绑定的是循环的数据，所以this.$refs对象里面有全部的数据
+      //热销榜:"[xxx]",单人精彩套餐:"[xxx]",冰爽饮品限时特惠:"[xxx]"...    this.$refs[itemText][0]获取的是数组里面的DOM元素
+      //因为itemText是变量，所以用[]
+      this.foodScroll.scrollToElement(element);
+      this.index1 = index;
+    },
+    /* leftMath() {
+        for(let i = 0; i<this.listHeight.length; i++){
+          let top = this.listHeight[i];
+          let bottom = this.listHeight[i+1];
+          if(!bottom || (this.scrollPosition >= top && this.scrollPosition < bottom))
+          {
+            this.index1 = i;  console.log(i)
+          }
+        }
+        return 0;
+      }, */
+    calculateHeight() {
+      var foodList = this.$refs.foods.getElementsByClassName("hook");
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
     }
+  },
+  created() {
+    this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
+    // class名称和每个特定图标编号相对应
+  },
+  mounted() {
+    this.getGoodsInfo();
   }
+};
 </script>
 
 <style lang="stylus" scoped>
---
+++ src/components/shopcar/Shopcar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>111</div>
+</template>
+<script>
+</script>
+<style lang="stylus" scoped></style>

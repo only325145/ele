2b498285bb92a85605eeedd2b1b6594e7fcfe788
only325145 <1251585756@qq.com>
2019-02-12 17:24:08 +0800

    start comment page

--- src/components/comments/Comments.vue
+++ src/components/comments/Comments.vue
@@ -1,9 +1,79 @@
 <template>
-  <div>This is comments page</div>
+  <div class="comment">
+    <div class="grade">
+      <div class="left">
+        <div class="point">3.9</div>
+        <div>综合评分</div>
+        <div class="compare">高于周边商家69.2%</div>
+      </div>
+      <div class="right">
+        <div class="star">
+          <span>商品评价</span>
+          <div class="starComp">
+            <Star :size="36" :score="4"></Star>
+          </div>
+        </div>
+        <div class="star">
+          <span>服务态度</span> 
+          <div class="starComp">
+            <Star :size="36" :score="4"></Star>
+          </div> 
+        </div>
+        <div>送达时间</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Star from "../star/Star.vue"
   export default {
-    name: "comments"
+    name: "comments",
+    components: {
+      Star
+    }
   }
 </script>
+
+<style lang="stylus" scoped>
+.comment
+  display: flex
+  position: absolute
+  top: 3.54rem
+  bottom: 1.16rem
+  width: 100%
+  overflow: hidden
+  .grade
+    width: 100%
+    padding: .36rem 0
+    .left
+      float: left
+      box-sizing: border-box
+      width: 40%
+      text-align: center
+      border-right: 1px solid rgba(147,153,159,0.2)
+      .point
+        font-size: .48rem
+        color: rgb(255,153,0)
+        line-height: .56rem
+        margin-bottom: .12rem
+      .compare
+        font-size: .24rem
+        color: rgb(147,153,159)
+        line-height: .24rem
+        margin-top: .16rem
+    .right
+      float: left
+      box-sizing: border-box
+      width: 60%
+      padding: 0 .48rem
+      .star
+        font-size: .24rem
+        color: rgb(7,17,27)
+        line-height: .36rem
+        margin: 0 .24rem .16rem 0
+        .starComp
+          display: inline-block
+          margin-left: .24rem
+          width: 2rem
+</style>
--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="detail" v-show="showFlag">
-    <div class="foodContent">
-      <div class="img">
-        <img class="pic" :src="food.icon" />
-      </div>
-      <div class="back" @click="goback">
-        <span class="iconfont">&#xe749;</span>
-      </div>
-      <div class="content">
-        <h1 class="foodName">{{food.name}}</h1>
-        <div class="extra">
-          <span class="count">月售{{food.sellCount}}</span>
-          <span>好评率{{food.rating}}%</span>  
-          <div class="price">
-            <span class="now">￥{{food.price}}</span>
-            <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
-          </div>
+    <div class="detailPart" >
+      <div class="foodContent">
+        <div class="img">
+          <img class="pic" :src="food.icon" />
         </div>
-        <div class="button" v-show="!food.count" @click="add">加入购物车</div>
-        <carcontrol class="select" v-show="food.count" :food="food"></carcontrol><!--将选中商品信息传给carcontrol组件-->
-      </div>
-      <div class="divide"></div>
-      <div class="infor">
-        <h1>商品介绍</h1>
-        <div class="information">{{info}}</div>
-      </div>
-      <div class="divide"></div>      
-    </div>  
+        <div class="content">
+          <h1 class="foodName">{{food.name}}</h1>
+          <div class="extra">
+            <span class="count">月售{{food.sellCount}}</span>
+            <span>好评率{{food.rating}}%</span>  
+            <div class="price">
+              <span class="now">￥{{food.price}}</span>
+              <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+            </div>
+          </div>
+          <div class="button" v-show="!food.count" @click="add">加入购物车</div>
+          <carcontrol class="select" v-show="food.count" :food="food"></carcontrol><!--将选中商品信息传给carcontrol组件-->
+        </div>
+        <div class="divide"></div>
+        <div class="infor">
+          <h1>商品介绍</h1>
+          <div class="information">{{info}}</div>
+        </div>
+        <div class="divide"></div>      
+      </div>  
+    </div>
+    <div class="back" @click="goback">
+      <span class="iconfont">&#xe749;</span>
+    </div>
   </div>
 </template>
 
@@ -41,7 +43,7 @@ export default {
   },
   data() {
     return {
-      showFlag: false,
+      showFlag: false
     };
   },
   methods: {
@@ -55,14 +57,13 @@ export default {
       if (!this.food.count) {
         Vue.set(this.food, "count", 1);
       }
-    },
+    }
   },
   computed: {
     info() {
-      if(this.food.info){
+      if (this.food.info) {
         return this.food.info;
-      }
-      else{
+      } else {
         return "无";
       }
     }
@@ -71,99 +72,104 @@ export default {
     Carcontrol
   },
   mounted() {
-    this.detailScroll = new BScroll(".detail", { click: true });
+    let wrapper = document.querySelector('.detailPart');   
+    this.detailScroll = new BScroll(wrapper, { click: true });  //此处将wrapper直接写成".detailPart"不能实现滚动，为什么？必须写成这种两行的格式
   }
 };
 </script>
 
 <style lang="stylus" scoped>
 .detail
+  width: 100%
   position: fixed
   z-index: 30
   top: 0
   bottom: 1.04rem
-  width: 100%
   background-color: white
-  .foodContent
-    .img
-      position: relative  //防止图片后加载形成抖动，先固定图片容器的大小
-      width: 100%
-      height: 0
-      padding-bottom: 100%
-      .pic
-        position: absolute   //将图片大小固定，和容器大小一样
-        top: 0
-        bottom: 0
+  .detailPart
+    width: 100%
+    height: 100%
+    overflow: hidden
+    .foodContent
+      .img
+        position: relative  //防止图片后加载形成抖动，先固定图片容器的大小
         width: 100%
-    .back
-      position: absolute
-      top: .2rem
-      left: .2rem
-      width: .7rem
-      height: .7rem
-      padding: .1rem
-      border-radius: 100%
-      background-color: rgba(0,0,0,0.3)
-      text-align: center
-      .iconfont
-        font-size: .5rem
-        color: #fff
-        line-height: .7rem
-    .content
-      position: relative
-      padding: .36rem
-      .foodName
-        font-size: .28rem
-        font-weight: 700
-        color: rgb(7,17,27)
-        line-height: .28rem
-        margin-bottom: .16rem
-      .extra 
+        height: 0
+        padding-bottom: 100%
+        .pic
+          position: absolute   //将图片大小固定，和容器大小一样
+          top: 0
+          bottom: 0
+          width: 100%
+      .content
         position: relative
-        line-height: .24rem
-        font-size: .24rem
-        color: rgb(147,153,159)
-        .count
-          margin-right: .24rem
-        .price
-          margin-top: .36rem
-          .now
-            font-size: .28rem
-            color: rgb(240,20,20)
-            font-weight: 700
-            line-height: .48rem
-            margin-right: .16rem
-          .old
-            text-decoration: line-through
-            font-weight: 700
-      .button
-        position: absolute
-        right: .36rem
-        bottom: .36rem
-        width: 1.24rem
-        height: .36rem
-        padding: .12rem .24rem
-        border-radius: .3rem
-        text-align: center
-        line-height: .36rem
-        font-size: .12rem
-        color: rgb(255,255,255)
-        background-color: rgb(0,160,220)
-      .select
-        position: absolute
-        right: .36rem
-        bottom: .36rem
-    .divide
-      width: 100%
-      height: .32rem
-      background-color: #f3f5f7
-      border-top: 1px solid rgba(7,17,27,0.1)
-      border-bottom: 1px solid rgba(7,17,27,0.1)
-    .infor
-      padding: .36rem
-      .information
-        padding: .12rem .16rem 0 .16rem
-        color: rgb(77,85,93)
-        font-weight: 200
-        line-height: .48rem
+        padding: .36rem
+        .foodName
+          font-size: .28rem
+          font-weight: 700
+          color: rgb(7,17,27)
+          line-height: .28rem
+          margin-bottom: .16rem
+        .extra 
+          position: relative
+          line-height: .24rem
+          font-size: .24rem
+          color: rgb(147,153,159)
+          .count
+            margin-right: .24rem
+          .price
+            margin-top: .36rem
+            .now
+              font-size: .28rem
+              color: rgb(240,20,20)
+              font-weight: 700
+              line-height: .48rem
+              margin-right: .16rem
+            .old
+              text-decoration: line-through
+              font-weight: 700
+        .button
+          position: absolute
+          right: .36rem
+          bottom: .36rem
+          width: 1.24rem
+          height: .36rem
+          padding: .12rem .24rem
+          border-radius: .3rem
+          text-align: center
+          line-height: .36rem
+          font-size: .12rem
+          color: rgb(255,255,255)
+          background-color: rgb(0,160,220)
+        .select
+          position: absolute
+          right: .36rem
+          bottom: .36rem
+      .divide
+        width: 100%
+        height: .32rem
+        background-color: #f3f5f7
+        border-top: 1px solid rgba(7,17,27,0.1)
+        border-bottom: 1px solid rgba(7,17,27,0.1)
+      .infor
+        padding: .36rem
+        .information
+          padding: .12rem .16rem 0 .16rem
+          color: rgb(77,85,93)
+          font-weight: 200
+          line-height: .48rem
+  .back
+    position: fixed 
+    top: .2rem
+    left: .2rem
+    width: .7rem
+    height: .7rem
+    padding: .1rem
+    border-radius: 100%
+    background-color: rgba(0,0,0,0.3)
+    text-align: center
+    .iconfont
+      font-size: .5rem
+      color: #fff
+      line-height: .7rem
 </style>
